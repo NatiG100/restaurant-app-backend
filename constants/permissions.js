5e625445e1e2985443adf63eb13196c198a7b2c8
NatiGK <natnaelgashu2022@gmail.com>
2023-01-12 12:01:30 +0300

    create user model

---
+++ constants/permissions.js
@@ -0,0 +1,10 @@
+export const allPermissions = [
+    "Manage Items",
+    "Manage Orders",
+    "Manage Users",
+    "Setting",
+    "View Info",
+    "View Items",
+    "View Orders",
+    "View Users",
+]
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,8 +1,32 @@
-const {Schema} = require('mongoose');
+const {Schema, Model} = require('mongoose');
 
+// user schema
 const UserSchema = new Schema({
     fullName:{
         type:String,
         required: true,
-    }
-})
+    },
+    previlages:{
+        type:[String],
+        required:true,
+    },
+    status:{
+        type:String,
+        default:"Active"
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    img:{
+        type:String,
+        default:"/default-avatar.jpeg"
+    },
+});
+
+
+
+//create user model from user schema
+const UserModel = Model('User', UserSchema);
+
+module.exports = UserModel;
