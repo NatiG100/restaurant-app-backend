0661657176e121a08e16a8e2310edb64f4c9e479
NatiG100 <natnaelgashu2022@gmail.com>
2023-01-15 09:20:16 +0300

    setup express session

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+require('dotenv').config()
+const {PORT,MONGO_DB_CONNECTION} = process.env;
+
 const express = require('express');
 const router = require('./routes/UserRoute');
 const app = express();
 
 const bodyParser = require('body-parser');
+const sessioin = require('express-session');
+const MongoDBStore = require('connect-mongodb-session');
+
+var store = new MongoDBStore({
+    uri:MONGO_DB_CONNECTION+"/restaurant-menu",
+    collection:'mySessions',
+})
+store.on('error',function(error){
+    console.log(error);
+});
+
+app.use(sessioin({
+    secret: 'This is a secret',
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    },
+    store: store,
+    resave: false,
+    saveUninitialized: false
+}));
+
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
