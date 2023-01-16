2b5198e9fd2bb29a00e1188e6ebacb19b06dff53
NatiG100 <natnaelgashu2022@gmail.com>
2023-01-16 15:43:09 +0300

    set sameSite true

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(cors({
 app.use(session({
     secret: 'This is a secret',
     cookie: {
+        sameSite:true,
       maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
     },
     store: store,
