3d4f0890763da15c4a734ec3f20cb626a019a436
NatiG100 <natnaelgashu2022@gmail.com>
2023-03-18 12:08:24 +0300

    protect user route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -10,23 +10,46 @@ const {
     DeleteAll 
 } = require('../controllers/UserControllers');
 const fileUpload = require('../utils/fileUpload');
+const requirePermission = require('../utils/requirePermission');
 
 //delete route for testing purpose
 UserRoute.delete('/delete-all/test',DeleteAll);
 
 //register user route
-UserRoute.post('/register',fileUpload('img/user/').single('img'),RegisterUser);
+UserRoute.post(
+    '/register',
+    requirePermission("Manage Users"),
+    fileUpload('img/user/').single('img'),
+    RegisterUser
+);
 
 //fetch all users
-UserRoute.get('/', FetchAllUsers);
+UserRoute.get(
+    '/', 
+    requirePermission("View Users"),
+    FetchAllUsers,
+);
 
 //fetch a user
-UserRoute.get('/:userId',FetchUser)
+UserRoute.get(
+    '/:userId',
+    requirePermission("View Users"),
+    FetchUser
+)
 
 //change status of a user
-UserRoute.patch('/:userId/change-status',ChangeUserStatus)
+UserRoute.patch(
+    '/:userId/change-status',
+    requirePermission("Manage Users"),
+    ChangeUserStatus,
+);
 
 //update user information
-UserRoute.patch('/:userId/update',fileUpload('img/user/').single('img'), UpdateUser)
+UserRoute.patch(
+    '/:userId/update',
+    requirePermission("Manage Users"),
+    fileUpload('img/user/').single('img'), 
+    UpdateUser
+);
 
 module.exports = UserRoute;
